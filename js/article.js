7148bece22bf1c0ae3b92291e0d14a56756dea63
unknown <julienfroger16@gmail.com>
2020-08-24 13:49:45 +0200

    MeP: js

---
+++ js/article.js
@@ -0,0 +1,7 @@
+const listArticle = document.getElementsByClassName()
+
+let numberArticle = document.getElementsByClassName(".number-article")
+let title = document.getElementsByClassName(".title")
+let summarry = document.getElementsByClassName(".summary")
+let price = document.getElementsByClassName(".price")
+
---
+++ js/dist/article.dev.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var listArticle = document.getElementsByClassName();
+var numberArticle = document.getElementsByClassName(".number-article");
+var title = document.getElementsByClassName(".title");
+var summarry = document.getElementsByClassName(".summary");
+var price = document.getElementsByClassName(".price");
